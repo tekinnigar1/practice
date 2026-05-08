a3053f32f573aad508e76cfb86cc13017c179234
tekinnigar1 <148658555+tekinnigar1@users.noreply.github.com>
2026-05-08 19:36:42 +0300

    add email project

---
+++ email/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
